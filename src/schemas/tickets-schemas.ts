0e7a899de7c451b914fb9b6bf6ed34ca473cd58f
Leonan Martins <leonanm04@gmail.com>
2023-04-19 11:55:34 -0300

    refactor: ticket schemas typed

--- src/schemas/tickets-schemas.ts
+++ src/schemas/tickets-schemas.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 import { ticketTypeIdInput } from '@/services';
 
-export const createTicketSchema = Joi.object(<ticketTypeIdInput>{
+export const createTicketSchema = Joi.object<ticketTypeIdInput>({
   ticketTypeId: Joi.number().required(),
 });
