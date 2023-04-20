04fb0408f5b46fec0a994da4bfb12508eb4f2d22
Leonan Martins <leonanm04@gmail.com>
2023-04-20 18:09:12 -0300

    fix: get/hotels/:id returning with rooms

--- src/repositories/hotels-repository/index.ts
+++ src/repositories/hotels-repository/index.ts
@@ -1,11 +1,16 @@
+import { Hotel, Room } from '@prisma/client';
 import { prisma } from '@/config';
 
-async function getHotels() {
+async function getHotels(): Promise<Hotel[]> {
   return prisma.hotel.findMany();
 }
 
-async function getHotelbyId(id: number) {
-  return prisma.hotel.findUnique({ where: { id } });
+async function getHotelbyId(id: number): Promise<
+  Hotel & {
+    Rooms: Room[];
+  }
+> {
+  return prisma.hotel.findUnique({ where: { id }, include: { Rooms: true } });
 }
 
 export const hotelsRepository = { getHotels, getHotelbyId };
